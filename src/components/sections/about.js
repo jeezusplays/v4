0816cc3e0ed7d170590a80c5c5d0683b647d3710
jeezusplays <68149788+jeezusplays@users.noreply.github.com>
2023-12-24 01:07:27 +0800

    Update about me

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['Python', 'Java', 'Vue', 'Flask', 'Node.js', 'HTML'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,31 +135,32 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Joey and I enjoy solving real-world problems using technology. My
+              interest in coding started back in 2018 when I decided to enrol into Information
+              Systems @ Temasek Polytechnic. It is the biggest leap of faith I have taken, and I
+              never regretted it!
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
+              <a href="https://www.corsivalab.com/sg/">a digital agency</a>,{' '}
+              <a href="https://www.tp.edu.sg/home.html">a polytechnic</a>, and{' '}
+              <a href="https://www.suss.edu.sg/">a local university</a>. Currently, I am looking
+              forward to working with more people and finding a job wih a company that I can grow
+              with.
+              {/* My main focus these days is building accessible, inclusive products and digital
               experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              clients. */}
             </p>
 
-            <p>
+            {/* <p>
               I also recently{' '}
               <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
                 launched a course
               </a>{' '}
               that covers everything you need to build a web app with the Spotify API using Node
               &amp; React.
-            </p>
+            </p> */}
 
             <p>Here are a few technologies I’ve been working with recently:</p>
           </div>
