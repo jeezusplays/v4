80fe5489ab38cddb37f3d3fcd30e9efb9d530fbf
jeezusplays <68149788+jeezusplays@users.noreply.github.com>
2023-12-26 21:04:52 +0800

    Update hero and footer

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,29 +60,35 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">Joey Tan.</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
+  // const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
+        I’m a penultimate university student at <a href="https://www.smu.edu.sg/">SMU</a>.
+        Currently, I'm focused on expanding my knowledge in IT and project management. I am also
+        looking for an internship at a company that I can grow with!
+        {/* I’m a software engineer specializing in building (and occasionally designing) exceptional
         digital experiences. Currently, I’m focused on building accessible, human-centered products
         at{' '}
         <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
           Upstatement
         </a>
-        .
+        . */}
       </p>
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
-    </a>
+    <>
+      <a
+        className="email-link"
+        href="https://www.linkedin.com/in/joey-tan-zuyi/"
+        target="_blank"
+        rel="noreferrer">
+        Check out my LinkedIn!
+      </a>
+    </>
   );
 
   const items = [one, two, three, four, five];
